8a5d048e3e6629e9cac5c130daf1239e22854a33
jorgeduje <julian.russo@team.wirall.com>
2022-11-09 11:09:03 -0300

    clase formularios y rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Formulario from "./components/pages/formulario/Formulario";
+import Home from "./components/pages/home/Home";
+import Login from "./components/pages/login/Login";
+
 
 function App() {
   return (
     <div className="App">
       
-      
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path="/" element={ <Formulario />} />
+          <Route path="/home" element={ <Home /> } />
+          <Route path="/login" element={ <Login /> } />
+
+        </Routes>
+
+      </BrowserRouter>
       
     </div>
   );
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>Estoy en el home</h1>
+  )
+}
+
+export default Home

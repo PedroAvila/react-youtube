c2e18a146f8b5316e1bddeb61ea326a8958c6c79
jorgeduje <julian.russo@team.wirall.com>
2022-11-14 12:21:35 -0300

    fetching de datos, axios y mapeo de arreglos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Characters from "./components/pages/characters/Characters";
+import Home from "./components/pages/home/Home";
 
 
 function App() {
   return (
-    <div className="App">
-      
-      
-      
-    </div>
+   <BrowserRouter>
+    <Routes>
+
+      <Route path="/" element={ <Home /> } />
+      <Route path="/characters" element={ <Characters /> } />
+
+    </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/characters/Characters.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Typography } from "@mui/material";
+import CardCharacter from "../../common/cardCharacter/CardCharacter";
+
+const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    // fetch("https://rickandmortyapi.com/api/character")
+    //     .then( res => res.json() )
+    //     .then( res => setCharacters(res.results))
+
+    axios.get("https://rickandmortyapi.com/api/character")
+      .then((res) => setCharacters(res.data.results));
+  }, []);
+
+  console.log(characters);
+
+  return (
+    <>
+      <Typography variant="h2" color="primary" align="center">
+        Personajes
+      </Typography>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+          padding: "20px",
+        }}
+      >
+        {characters.map((character) => {
+          return <CardCharacter character={character} key={character.id}/>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Characters;
